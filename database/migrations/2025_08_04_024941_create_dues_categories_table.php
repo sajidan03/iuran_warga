b7a062109f575a8098d828dc1fbcf179160a6a73
sajidan03 <rifansyahsajidan03@gmail.com>
2025-08-04 09:55:41 +0700

    beres migration

---
+++ database/migrations/2025_08_04_024941_create_dues_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dues_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->enum('period', ['mingguan', 'bulanan', 'tahunan']);
+            $table->integer('nominal');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dues_categories');
+    }
+};
