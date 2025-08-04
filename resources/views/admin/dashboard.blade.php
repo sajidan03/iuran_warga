88f4f071cce9aca7c83cca704acc68f08d06cbac
dhiyakasep <akunhalal2023@gmail.com>
2025-08-04 09:58:52 +0700

    nge root

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,9 @@
 @extends('admin.templateAdmin')
+@section('content')
+<div class="container py-4">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h3 class="fw-bold text-success">
+            Dashboard Admin
+        </h3>
+    </div>
+</div>
