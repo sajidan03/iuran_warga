c03f7483c0f5f8b81bd6237ca9ec3921063ee374
dhiyakasep <akunhalal2023@gmail.com>
2025-08-04 09:48:36 +0700

    templateAdmin

---
+++ resources/views/admin/templateAdmin.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm bg-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link">A</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">B</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">C</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
