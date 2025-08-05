86c8448eafc2f54920fd3f4d7d2021dd38d06e20
dhiyakasep <akunhalal2023@gmail.com>
2025-08-05 07:51:35 +0700

    isian

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard');
 });
+
+Route::get('/login', function () {
+    return view('login.login');
+});
