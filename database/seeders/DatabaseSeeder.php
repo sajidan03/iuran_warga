d07d8f009a5d52dea32bc1f26ba07c3a85be813a
fureiyaa <farizdzu16@gmail.com>
2025-08-05 09:04:10 +0700

    back

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'name' => 'Sajidan',
             'username' => 'sajidan',
+            'password' => bcrypt('sajidan123'),
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\WargaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin.dashboard');
+    return view('warga.dashboard');
 });
 
 Route::get('/login', [LoginController::class, 'loginView'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::get('/dashboard', [WargaController::class, 'dashboard'])->name('warga.dashboard')->middleware('auth');
+Route::get('/admin/dashboard', [AdminController::class, 'dashboardAdmin']);
