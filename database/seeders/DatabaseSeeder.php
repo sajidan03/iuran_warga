7cf3e690e1af5a15cf9ef4a8707982fa319497cf
dhiyakasep <akunhalal2023@gmail.com>
2025-08-06 08:51:31 +0700

    profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Sajidan',
             'username' => 'sajidan',
             'password' => bcrypt('sajidan123'),
+            'foto' => 'firdaus sayang amel'
         ]);
     }
 }
